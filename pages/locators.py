41e9fd976f4a21626af80693f77c22fd6813cc9b
Anna Kapustina <annakps777@gmail.com>
2026-01-09 01:38:56 +0300

    Move login link actions to BasePage and add product page login tests

--- pages/locators.py
+++ pages/locators.py
@@ -19,5 +19,10 @@ class ProductPageLocators:
 
     BASKET_TOTAL = (By.CSS_SELECTOR, "#messages .alert-info strong")
 
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
 
 
