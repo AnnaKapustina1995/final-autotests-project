81bd6efd50885c86ee2ce3c999da053207d878ce
Anna Kapustina <annakps777@gmail.com>
2026-01-09 00:00:24 +0300

    Make product page checks independent from product data

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,12 +6,19 @@ class ProductPage(BasePage):
     def add_to_basket(self):
         self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON).click()
 
-    def should_be_success_message_with_correct_product_name(self):
-        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
-        success_name = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE_PRODUCT_NAME).text
-        assert product_name == success_name, "Product name in success message is incorrect"
+    # --- getters (берём данные со страницы) ---
+    def get_product_name(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
 
-    def should_be_basket_total_equal_product_price(self):
-        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+    def get_product_price(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+
+    # --- checks (принимают ожидаемые данные) ---
+    def should_be_success_message_with_product_name(self, expected_name):
+        actual_name = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE_PRODUCT_NAME).text
+        assert actual_name == expected_name, "Product name in success message is incorrect"
+
+    def should_be_basket_total_equal_price(self, expected_price):
         basket_total = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL).text
-        assert product_price == basket_total, "Basket total is not equal to product price"
+        assert basket_total == expected_price, "Basket total is not equal to product price"
+
